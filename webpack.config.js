d864f3e7ec9519b113a43b15ff9889d49935404f
Spencer Finnell <spencerfinnell@gmail.com>
2019-01-16 09:11:17 -0500

    Update for file-loader v3

--- webpack.config.js
+++ webpack.config.js
@@ -65,8 +65,9 @@ const config = {
 						options: {
 							limit: 8000, // Convert images < 8kb to base64 strings
 							name: '[name].[ext]',
-							useRelativePath: true,
-							outputPath: './public/',
+							outputPath: ( url, resourcePath, context ) => {
+								return `./public/${ path.relative( context, resourcePath ).replace( 'resources/assets', '' ) }`
+							}
 						},
 					},
 				],
